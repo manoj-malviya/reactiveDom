b29b70159c3c20fb1437204a8b03ba045f727e5a
Manoj Malviya <manojm@girnarsoft.com>
2025-09-22 16:52:57 +0000

    first commit

---
+++ signal.js
@@ -0,0 +1,60 @@
+// Signal
+let subscriber = null;
+
+function signal(initialValue) {
+  let value = initialValue;
+  const subscriptions = new Set();
+  const watchers = new Set();
+
+  return {
+    get value() {
+      if (subscriber) {
+        subscriptions.add(subscriber);
+      }
+      return value;
+    },
+
+    set value(updated) {
+      if (value !== updated) {
+        const oldValue = value;
+        value = updated;
+        subscriptions.forEach((callback) => callback());
+        watchers.forEach((callback) => callback(oldValue, updated));
+      }
+    },
+
+    addWatcher(callback) {
+      watchers.add(callback);
+      return () => watchers.delete(callback);
+    },
+  };
+}
+
+function effect(callback) {
+  subscriber = callback;
+  callback();
+  subscriber = null;
+}
+
+function compute(callback) {
+  const newSig = signal();
+  effect(() => {
+    newSig.value = callback();
+  });
+
+  return newSig;
+}
+
+function watch(signal, callback) {
+  const handler = (oldValue, newValue) => {
+    callback(oldValue, newValue);
+  };
+  const unsubscribe = signal.addWatcher(handler);
+  return unsubscribe;
+}
+
+function createAppState() {
+  const state = {};
+  
+  return state;
+}
